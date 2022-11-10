530a53b7734af49391842cc6b92564bbdcaf5a86
Mavrik <r.mavrik@gmail.com>
2022-11-10 08:47:13 +0100

    start eval

---
+++ EViOS5/EViOS5App.swift
@@ -0,0 +1,17 @@
+//
+//  EViOS5App.swift
+//  EViOS5
+//
+//  Created by Mavrik on 10/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct EViOS5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
